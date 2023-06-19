5f07985f6f9b596984691037e515439033e67e07
SajaMatar <sm6r@example.com>
2023-06-19 16:47:51 +0300

    py code for insertion sort

---
+++ Foundation/getting_started/insertion_sort/code.py
@@ -0,0 +1,13 @@
+def insertionSort(array=[]):
+    for x in range(1, len(array), 1):
+        key = array[x]
+        i = x - 1
+        while i > -1 and array[i] > key:
+            array[i+1] = array[i]
+            i = i - 1
+        array[i+1] = key
+
+
+mylist = [30, 10, 7, 60, 55]
+insertionSort(mylist)
+print(mylist)
